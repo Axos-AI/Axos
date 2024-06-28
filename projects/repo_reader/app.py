1a2e147c07a2d98fd7e2305940f9b12a4d2cf99d
vjz3qz <vjz3qz@virginia.edu>
2024-06-28 11:32:52 -0700

    add args

--- projects/repo_reader/app.py
+++ projects/repo_reader/app.py
@@ -1,5 +1,15 @@
 # app.py
 from src.main import main
+import argparse
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", '--chat', action='store_false', help='Run the chat interface')
+    parser.add_argument("-r", '--refactor', action='store_false', help='Run the refactoring AI')
+    args = parser.parse_args()
+    main(args.chat, args.refactor)
+
+
+
+
+
