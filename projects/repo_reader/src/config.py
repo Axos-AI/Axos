d2e759f2014c700efbdb95dcf9767ea6a1c49e01
vjz3qz <vjz3qz@virginia.edu>
2024-06-28 11:32:46 -0700

    update config

--- projects/repo_reader/src/config.py
+++ projects/repo_reader/src/config.py
@@ -1,5 +1,11 @@
 #config.py
+import os
+from dotenv import load_dotenv
+
 WHITE = "\033[37m"
 GREEN = "\033[32m"
 RESET_COLOR = "\033[0m"
 model_name = "gpt-4o"
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
