542dc52da5353129b0c9657f0551cdcb32264ce9
vjz3qz <vjz3qz@virginia.edu>
2024-06-28 12:27:45 -0700

    refactor: Update command line arguments for chat and refactor options

--- projects/repo_reader/app.py
+++ projects/repo_reader/app.py
@@ -4,8 +4,8 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", '--chat', action='store_false', help='Run the chat interface')
-    parser.add_argument("-r", '--refactor', action='store_false', help='Run the refactoring AI')
+    parser.add_argument("-c", '--chat', action='store_true', help='Run the chat interface')
+    parser.add_argument("-r", '--refactor', action='store_true', help='Run the refactoring AI')
     args = parser.parse_args()
     if not args.chat and not args.refactor:
         args.chat = True
