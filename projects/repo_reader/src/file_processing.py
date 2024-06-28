3f25af353853e8cce99d8b9dbb8327258ee441fb
vjz3qz <vjz3qz@virginia.edu>
2024-06-27 23:16:01 -0700

    update

--- projects/repo_reader/src/file_processing.py
+++ projects/repo_reader/src/file_processing.py
@@ -5,9 +5,9 @@ import subprocess
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from rank_bm25 import BM25Okapi
-from langchain.document_loaders import DirectoryLoader, NotebookLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from src.utils import clean_and_tokenize
+from langchain_community.document_loaders import DirectoryLoader, NotebookLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def clone_github_repo(github_url, local_path):
     try:
