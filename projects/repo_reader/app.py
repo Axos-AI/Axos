cc6d9301a0bebd1c29d1c746a68c8280d448189d
vjz3qz <vjz3qz@virginia.edu>
2024-06-29 18:15:24 -0700

    add

--- projects/repo_reader/app.py
+++ projects/repo_reader/app.py
@@ -6,7 +6,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", '--chat', action='store_true', help='Run the chat interface')
     parser.add_argument("-r", '--refactor', action='store_true', help='Run the refactoring AI')
+    parser.add_argument("-e", '--entry_point', type=str, help='The entry point file to start the refactoring from the root of the repository.')
     args = parser.parse_args()
+    if args.entry_point:
+        args.refactor = True
+    if not args.entry_point and args.refactor:
+        print("Please provide an entry point file to start the refactoring.")
+        exit()
     if not args.chat and not args.refactor:
         args.chat = True
     main(args.chat, args.refactor)
